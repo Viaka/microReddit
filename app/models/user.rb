490e16aea15099af68be75e12779753ea43e65ab
Ross <Rharper9@gmail.com>
2020-04-18 15:23:27 -0500

    Added comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     validates :username, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
     validates :username, length: {minimum: 2}
-    
+
     has_many :posts
+    has_many :comments
 end
