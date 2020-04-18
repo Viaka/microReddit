448877b165ef9266eeef04dbe0cce29c07e2003e
Ross <Rharper9@gmail.com>
2020-04-18 15:05:26 -0500

    added users and associated posts

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    validates :body, presence: true    
+    validates :title, presence: true 
+    
+    belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :username, presence: true
+    validates :username, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+    validates :username, length: {minimum: 2}
+    
+    has_many :posts
+end
